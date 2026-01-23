208c36700db65bc66e25fd2ab38023951833cbb7
Antony Miguel Nequiz Meza <nekixito@gmail.com>
2026-01-23 12:25:17 -0600

    Uso de constructores.

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -20,19 +20,11 @@ public class Main {
         int duracion = ScannersUtils.capturarNumero("Duracion del contenido");
         double calificacion =  ScannersUtils.capturarDecimal("Calificación del contenido");
 
-        Pelicula pelicula = new Pelicula();
-        pelicula.titulo = nombre;
-        pelicula.fechaEstreno = LocalDate.of(2018,10,15);
-        pelicula.genero = genero;
-        pelicula.calificar(calificacion);
-        pelicula.duracion = duracion;
+        Pelicula pelicula = new Pelicula(nombre, duracion, genero, calificacion);
 
         System.out.println(pelicula.obtenerFichaTecnica());
 
-        Usuario usuario = new Usuario();
-        usuario.nombre = "Miguel";
-        usuario.fechaRegistro = LocalDateTime.of(2025,12,24,17,15,14);
-
+        Usuario usuario = new Usuario("Miguel","miguel@mail.com");
         usuario.ver(pelicula);
 
     }
